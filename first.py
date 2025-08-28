48b07ec2094516e1d9c1d045f591eb9ae37aab1a
bishtebisht <bhoomibisht007@gmail.com>
2025-08-28 20:32:27 +0530

    variables

--- first.py
+++ first.py
@@ -1,1 +1,72 @@
-print("hello world");
+# i=1
+# print(type(i))
+# print(id(i))
+
+#multiple variable
+# a,b,c=1,2,3
+# print(a,b,c)
+
+# print(id(a))
+# print(id(b))
+# print(id(c))
+
+# a=b=c
+# print(id(a))
+# print(id(b))
+# print(id(c)) #(same as c)
+
+#variable name-
+# 1-camel style:- myVariable
+# 2-snake style:- my_variable
+# 3-pascal style:- MyVariable
+
+# NUMERIC DATATYPE
+# var1=1   
+# var2=True
+# var3=10.08
+# var4=10+9j
+
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+
+# OUTPUT
+# <class 'int'>
+# <class 'bool'>
+# <class 'float'>
+# <class 'complex'>
+
+#STRING DATATYPE
+#(multiline string)
+# print("hello world")
+# a="""name-Bhoomi Bisht
+# course-Btech
+# branch-CSE"""
+# print(a)
+
+# #in
+# print("Btech" in a)  #True
+# if "Bhoomi" in a:
+#     print("yes")
+# else: 
+#     print("no")
+
+# print("yes" not in a)
+
+# str="Hello"
+# print(str[:])
+# print(str[2:4])
+# print(str[2:])
+# print(str[:4])
+# print(str[-4:-2])
+#hello
+# ll
+# llo
+# hell
+# el
+# print(str.upper())  #HELLO
+# print(str.lower())  #HELLO
+
+a=" bhoomi "
+print(a.strip()) #removes spaces
